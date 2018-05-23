0ffb139edfcdabbe8db9e41a6e72b220291c4cbe
Sami Tolvanen <samitolvanen@google.com>
2018-05-23 13:27:08 -0700

    FROMLIST: scs: add accounting
    This change adds accounting for the memory allocated for shadow stacks.
    
    Bug: 145210207
    Change-Id: I51157fe0b23b4cb28bb33c86a5dfe3ac911296a4
    (am from https://lore.kernel.org/patchwork/patch/1149055/)
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4893,6 +4893,9 @@ void show_free_areas(unsigned int filter, nodemask_t *nodemask)
 			" managed:%lukB"
 			" mlocked:%lukB"
 			" kernel_stack:%lukB"
+#ifdef CONFIG_SHADOW_CALL_STACK
+			" shadow_call_stack:%lukB"
+#endif
 			" pagetables:%lukB"
 			" bounce:%lukB"
 #ifdef CONFIG_ZSMALLOC
@@ -4917,6 +4920,9 @@ void show_free_areas(unsigned int filter, nodemask_t *nodemask)
 			K(zone->managed_pages),
 			K(zone_page_state(zone, NR_MLOCK)),
 			zone_page_state(zone, NR_KERNEL_STACK_KB),
+#ifdef CONFIG_SHADOW_CALL_STACK
+			zone_page_state(zone, NR_KERNEL_SCS_BYTES) / 1024,
+#endif
 			K(zone_page_state(zone, NR_PAGETABLE)),
 			K(zone_page_state(zone, NR_BOUNCE)),
 #ifdef CONFIG_ZSMALLOC
