fd7652e6241c3b74fd18548a9385473044df6d70
Geert Uytterhoeven <geert+renesas@glider.be>
2018-10-11 10:42:49 +0200

    UPSTREAM: lib/vsprintf: Hash printed address for netdev bits fallback
    The handler for "%pN" falls back to printing the raw pointer value when
    using a different format than the (sole supported) special format
    "%pNF", potentially leaking sensitive information regarding the kernel
    layout in memory.
    
    Avoid this leak by printing the hashed address instead.
    Note that there are no in-tree users of the fallback.
    
    Bug: 78533979
    Fixes: ad67b74d2469d9b8 ("printk: hash addresses printed with %p")
    Link: http://lkml.kernel.org/r/20181011084249.4520-4-geert+renesas@glider.be
    To: "Tobin C . Harding" <me@tobin.cc>
    To: Andrew Morton <akpm@linux-foundation.org>
    To: Jonathan Corbet <corbet@lwn.net>
    Cc: linux-doc@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Petr Mladek <pmladek@suse.com>
    (cherry picked from commit 431bca24308c5a82575571023a7128a881f716b2)
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I0e42ce10466947489359b33aad5413589e619dcf

--- lib/vsprintf.c
+++ lib/vsprintf.c
@@ -1423,7 +1423,8 @@ char *restricted_pointer(char *buf, char *end, const void *ptr,
 }
 
 static noinline_for_stack
-char *netdev_bits(char *buf, char *end, const void *addr, const char *fmt)
+char *netdev_bits(char *buf, char *end, const void *addr,
+		  struct printf_spec spec,  const char *fmt)
 {
 	unsigned long long num;
 	int size;
@@ -1434,9 +1435,7 @@ char *netdev_bits(char *buf, char *end, const void *addr, const char *fmt)
 		size = sizeof(netdev_features_t);
 		break;
 	default:
-		num = (unsigned long)addr;
-		size = sizeof(unsigned long);
-		break;
+		return ptr_to_id(buf, end, addr, spec);
 	}
 
 	return special_hex_number(buf, end, num, size);
@@ -1868,7 +1867,7 @@ char *pointer(const char *fmt, char *buf, char *end, void *ptr,
 	case 'K':
 		return restricted_pointer(buf, end, ptr, spec);
 	case 'N':
-		return netdev_bits(buf, end, ptr, fmt);
+		return netdev_bits(buf, end, ptr, spec, fmt);
 	case 'a':
 		return address_val(buf, end, ptr, fmt);
 	case 'd':
