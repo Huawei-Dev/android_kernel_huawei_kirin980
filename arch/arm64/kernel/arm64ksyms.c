c72f1ff5156b14712fa3ae3af4966cc7b0ebb892
Andrey Ryabinin <aryabinin@virtuozzo.com>
2018-10-26 15:02:30 -0700

    UPSTREAM: arm64: lib: use C string functions with KASAN enabled
    Upstream commit 19a2ca0fb560fd7be7b5293c6b652c6d6078dcde.
    
    ARM64 has asm implementation of memchr(), memcmp(), str[r]chr(),
    str[n]cmp(), str[n]len().  KASAN don't see memory accesses in asm code,
    thus it can potentially miss many bugs.
    
    Ifdef out __HAVE_ARCH_* defines of these functions when KASAN is enabled,
    so the generic implementations from lib/string.c will be used.
    
    We can't just remove the asm functions because efistub uses them.  And we
    can't have two non-weak functions either, so declare the asm functions as
    weak.
    
    Link: http://lkml.kernel.org/r/20180920135631.23833-2-aryabinin@virtuozzo.com
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Reported-by: Kyeongdon Kim <kyeongdon.kim@lge.com>
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>
    Change-Id: I64445ac33926c8fa614c3861440e1737a11575e0
    Bug: 128674696

--- arch/arm64/kernel/arm64ksyms.c
+++ arch/arm64/kernel/arm64ksyms.c
@@ -44,12 +44,17 @@ EXPORT_SYMBOL(memstart_addr);
 extern s64 phystart_addr;
 EXPORT_SYMBOL(phystart_addr);
 	/* string / mem functions */
+#ifndef CONFIG_KASAN
 EXPORT_SYMBOL(strchr);
 EXPORT_SYMBOL(strrchr);
 EXPORT_SYMBOL(strcmp);
 EXPORT_SYMBOL(strncmp);
 EXPORT_SYMBOL(strlen);
 EXPORT_SYMBOL(strnlen);
+EXPORT_SYMBOL(memcmp);
+EXPORT_SYMBOL(memchr);
+#endif
+
 EXPORT_SYMBOL(memset);
 EXPORT_SYMBOL(memcpy);
 EXPORT_SYMBOL(memcpy_reg);
@@ -57,8 +62,6 @@ EXPORT_SYMBOL(memmove);
 EXPORT_SYMBOL(__memset);
 EXPORT_SYMBOL(__memcpy);
 EXPORT_SYMBOL(__memmove);
-EXPORT_SYMBOL(memchr);
-EXPORT_SYMBOL(memcmp);
 
 	/* atomic bitops */
 EXPORT_SYMBOL(set_bit);
