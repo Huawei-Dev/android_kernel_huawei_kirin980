267f715c3df04c3d365fe54447af1ba1d83e2fd2
Beniamin Bia <biabeniamin@gmail.com>
2019-02-01 17:01:38 +0200

    staging: iio: frequency: ad9833: Load clock using clock framework
    [ Upstream commit 8e8040c52e63546d1171c188a24aacf145a9a7e0 ]
    
    The clock frequency is loaded from device-tree using clock framework
    instead of statically value. The change allow configuration of
    the device via device-trees and better initialization sequence.
    This is part of broader effort to add device-tree support to this driver
    and take it out from staging.
    
    Signed-off-by: Beniamin Bia <beniamin.bia@analog.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Stable-dep-of: b48aa9917589 ("staging: iio: frequency: ad9834: Validate frequency parameter value")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit a6316b6f127a877285c83d2ed45b20e6712e6d1b)
    Signed-off-by: Vegard Nossum <vegard.nossum@oracle.com>
    Signed-off-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>

--- drivers/staging/iio/frequency/ad9834.c
+++ drivers/staging/iio/frequency/ad9834.c
@@ -116,7 +116,7 @@ static int ad9834_write_frequency(struct ad9834_state *st,
 
 	clk_freq = clk_get_rate(st->mclk);
 
-	if (!clk_freq || fout > (clk_freq / 2))
+	if (fout > (clk_freq / 2))
 		return -EINVAL;
 
 	regval = ad9834_calc_freqreg(clk_freq, fout);
@@ -419,7 +419,14 @@ static int ad9834_probe(struct spi_device *spi)
 	spi_set_drvdata(spi, indio_dev);
 	st = iio_priv(indio_dev);
 	mutex_init(&st->lock);
-	st->mclk = 25000000;
+	st->mclk = devm_clk_get(&spi->dev, NULL);
+
+	ret = clk_prepare_enable(st->mclk);
+	if (ret) {
+		dev_err(&spi->dev, "Failed to enable master clock\n");
+		goto error_disable_reg;
+	}
+
 	st->spi = spi;
 	st->devid = spi_get_device_id(spi)->driver_data;
 	st->reg = reg;
