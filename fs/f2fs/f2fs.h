2ad3321ad2d9939d57b13fd7e067cd43a2a3f06e
Chao Yu <yuchao0@huawei.com>
2019-04-15 15:26:31 +0800

    f2fs: fix wrong __is_meta_io() macro
    This patch changes codes as below:
    - don't use is_read_io() as a condition to judge the meta IO.
    - use .is_por to replace .is_meta to indicate IO is from recovery explicitly.
    
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -1231,7 +1231,7 @@ struct f2fs_io_info {
 	bool submitted;		/* indicate IO submission */
 	int need_lock;		/* indicate we need to lock cp_rwsem */
 	bool in_list;		/* indicate fio is in io_list */
-	bool is_meta;		/* indicate borrow meta inode mapping or not */
+	bool is_por;		/* indicate IO is from recovery or not */
 	bool retry;		/* need to reallocate block address */
 	enum iostat_type io_type;	/* io type */
 	struct writeback_control *io_wbc; /* writeback control */
@@ -3299,6 +3299,8 @@ static inline void f2fs_update_iostat(struct f2fs_sb_info *sbi,
 
 #define __is_large_section(sbi)		((sbi)->segs_per_sec > 1)
 
+#define __is_meta_io(fio) (PAGE_TYPE_OF_BIO((fio)->type) == META)
+
 static inline block_t fs_free_space_threshold(struct f2fs_sb_info *sbi)
 {
         return (block_t)(SM_I(sbi)->main_segments * sbi->blocks_per_seg *
@@ -3310,9 +3312,6 @@ static inline block_t device_free_space_threshold(struct f2fs_sb_info *sbi)
         return (block_t)(SM_I(sbi)->main_segments * sbi->blocks_per_seg *
                                         DEVICE_FREE_SPACE_PERCENT) / 100;
 }
-
-#define __is_meta_io(fio) (PAGE_TYPE_OF_BIO((fio)->type) == META &&	\
-				(!is_read_io((fio)->op) || (fio)->is_meta))
 
 bool f2fs_is_valid_blkaddr(struct f2fs_sb_info *sbi,
 					block_t blkaddr, int type);
