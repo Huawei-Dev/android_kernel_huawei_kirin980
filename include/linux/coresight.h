1d8125e4ac734ee8afe381882724e334fc8db095
Mathieu Poirier <mathieu.poirier@linaro.org>
2019-04-25 13:53:01 -0600

    UPSTREAM: coresight: Communicate perf event to sink buffer allocation functions
    Make struct perf_event available to sink buffer allocation functions in
    order to use the pid they carry to allocate and free buffer memory along
    with regimenting access to what source a sink can collect data for.
    
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Tested-by: Leo Yan <leo.yan@linaro.org>
    Tested-by: Robert Walker <robert.walker@arm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    (Upstream commit a0f08a6a9fee0ce3f2d36e3e30799e4c1ec171f4).
    
    Bug: 140266694
    Change-Id: I3f1f16ebef75ec3aa629c46e3f1dc70f3a5ccb91
    Signed-off-by: Yabin Cui <yabinc@google.com>

--- include/linux/coresight.h
+++ include/linux/coresight.h
@@ -231,8 +231,9 @@ struct coresight_ops_sink {
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4,1,0)
 	int (*enable)(struct coresight_device *csdev, u32 mode, void *data);
 	int (*disable)(struct coresight_device *csdev);
-	void *(*alloc_buffer)(struct coresight_device *csdev, int cpu,
-			      void **pages, int nr_pages, bool overwrite);
+	void *(*alloc_buffer)(struct coresight_device *csdev,
+			      struct perf_event *event, void **pages,
+			      int nr_pages, bool overwrite);
 	void (*free_buffer)(void *config);
 	unsigned long (*update_buffer)(struct coresight_device *csdev,
 			      struct perf_output_handle *handle,
