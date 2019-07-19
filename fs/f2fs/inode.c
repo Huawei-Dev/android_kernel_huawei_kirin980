abefbca4e31ab7a40ef7a0d373430cd7488386a5
Chao Yu <yuchao0@huawei.com>
2019-07-19 11:51:11 +0800

    f2fs: fix to avoid tagging SBI_QUOTA_NEED_REPAIR incorrectly
    On a quota disabled image, with fault injection, SBI_QUOTA_NEED_REPAIR
    will be set incorrectly in error path of f2fs_evict_inode(), fix it.
    
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/inode.c
+++ fs/f2fs/inode.c
@@ -770,7 +770,8 @@ retry:
 
 	if (err) {
 		f2fs_update_inode_page(inode);
-		set_sbi_flag(sbi, SBI_QUOTA_NEED_REPAIR);
+		if (dquot_initialize_needed(inode))
+			set_sbi_flag(sbi, SBI_QUOTA_NEED_REPAIR);
 	}
 
 	if (unlikely(is_inode_flag_set(inode, FI_DIRTY_INODE))) {
