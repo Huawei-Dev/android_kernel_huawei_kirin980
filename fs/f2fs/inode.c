e9a4a666503fb6f489664b61c388fe8762f6c0e6
Chao Yu <yuchao0@huawei.com>
2019-08-15 19:45:35 +0800

    f2fs: use wrapped f2fs_cp_error()
    Just cleanup, no logic change.
    
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/inode.c
+++ fs/f2fs/inode.c
@@ -801,7 +801,7 @@ no_delete:
 		f2fs_bug_on(sbi, 1);
 	}
 
-	if (likely(!is_set_ckpt_flags(sbi, CP_ERROR_FLAG) &&
+	if (likely(!f2fs_cp_error(sbi) &&
 				!is_sbi_flag_set(sbi, SBI_CP_DISABLED)))
 		f2fs_bug_on(sbi, is_inode_flag_set(inode, FI_DIRTY_INODE));
 	else
