3ffcd646f6fc1c2f9dc55af9fb58cceb19a6823d
Roger Quadros <rogerq@ti.com>
2019-09-05 10:17:45 -0600

    usb: dwc3: Allow disabling of metastability workaround
    commit 42bf02ec6e420e541af9a47437d0bdf961ca2972 upstream
    
    Some platforms (e.g. TI's DRA7 USB2 instance) have more trouble
    with the metastability workaround as it supports only
    a High-Speed PHY and the PHY can enter into an Erratic state [1]
    when the controller is set in SuperSpeed mode as part of
    the metastability workaround.
    
    This causes upto 2 seconds delay in enumeration on DRA7's USB2
    instance in gadget mode.
    
    If these platforms can be better off without the workaround,
    provide a device tree property to suggest that so the workaround
    is avoided.
    
    [1] Device mode enumeration trace showing PHY Erratic Error.
         irq/90-dwc3-969   [000] d...    52.323145: dwc3_event: event (00000901): Erratic Error [U0]
         irq/90-dwc3-969   [000] d...    52.560646: dwc3_event: event (00000901): Erratic Error [U0]
         irq/90-dwc3-969   [000] d...    52.798144: dwc3_event: event (00000901): Erratic Error [U0]
    
    Signed-off-by: Roger Quadros <rogerq@ti.com>
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -1188,6 +1188,9 @@ static void dwc3_get_properties(struct dwc3 *dwc)
 	dwc->gctl_reset_quirk = device_property_read_bool(dev,
 				"snps,gctl-reset-quirk");
 
+	dwc->dis_metastability_quirk = device_property_read_bool(dev,
+				"snps,dis_metastability_quirk");
+
 	dwc->lpm_nyet_threshold = lpm_nyet_threshold;
 	dwc->tx_de_emphasis = tx_de_emphasis;
 
--- drivers/usb/dwc3/core.h
+++ drivers/usb/dwc3/core.h
@@ -911,6 +911,7 @@ struct dwc3_scratchpad_array {
  * 	1	- -3.5dB de-emphasis
  * 	2	- No de-emphasis
  * 	3	- Reserved
+ * @dis_metastability_quirk: set to disable metastability quirk.
  * @imod_interval: set the interrupt moderation interval in 250ns
  *                 increments or 0 to disable.
  */
@@ -1078,6 +1079,8 @@ struct dwc3 {
 	unsigned		pcd_suspended:1;
 	unsigned		dis_split_quirk:1;
 	unsigned		gctl_reset_quirk:1;
+
+	unsigned		dis_metastability_quirk:1;
 
 	u16			imod_interval;
 };
