ccefba1a0f4c299e8e57f8bef0d8feaa31ec1b1c
Yonghong Song <yhs@fb.com>
2020-05-09 10:59:10 -0700

    BACKPORT: net: bpf: Add netlink and ipv6_route bpf_iter targets
    This patch added netlink and ipv6_route targets, using
    the same seq_ops (except show() and minor changes for stop())
    for /proc/net/{netlink,ipv6_route}.
    
    The net namespace for these targets are the current net
    namespace at file open stage, similar to
    /proc/net/{netlink,ipv6_route} reference counting
    the net namespace at seq_file open stage.
    
    Since module is not supported for now, ipv6_route is
    supported only if the IPV6 is built-in, i.e., not compiled
    as a module. The restriction can be lifted once module
    is properly supported for bpf_iter.
    
    Change-Id: Id907ac146e81197abd185e0d6b7431d176dca355
    Signed-off-by: Yonghong Song <yhs@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Andrii Nakryiko <andriin@fb.com>
    Link: https://lore.kernel.org/bpf/20200509175910.2476329-1-yhs@fb.com

--- include/linux/proc_fs.h
+++ include/linux/proc_fs.h
@@ -45,6 +45,9 @@ extern void sched_hwstatus_qos_enqueue(struct task_struct *task, int qos_type);
 extern void sched_hwstatus_qos_dequeue(struct task_struct *task, int qos_type);
 #endif
 
+extern int bpf_iter_init_seq_net(void *priv_data);
+extern void bpf_iter_fini_seq_net(void *priv_data);
+
 #else /* CONFIG_PROC_FS */
 
 static inline void proc_root_init(void)
