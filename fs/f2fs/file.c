cb2d50d5b02d730a31226e78ce53db6511056c31
LuK1337 <priv.luk@gmail.com>
2020-07-31 16:22:46 +0200

    f2fs: disable F2FS_IOC_[G|S]ET_PIN_FILE ioctl commands
    * These aren't really working that well with uncrypt @ 9,
      disabling them lets us actually use OTA.
    
    Change-Id: Icd7dd5e0d475cb1cebd9d1bd7d5bbc83bdb85fd8

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -3110,10 +3110,6 @@ long f2fs_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		return f2fs_ioc_fsgetxattr(filp, arg);
 	case F2FS_IOC_FSSETXATTR:
 		return f2fs_ioc_fssetxattr(filp, arg);
-	case F2FS_IOC_GET_PIN_FILE:
-		return f2fs_ioc_get_pin_file(filp, arg);
-	case F2FS_IOC_SET_PIN_FILE:
-		return f2fs_ioc_set_pin_file(filp, arg);
 	case F2FS_IOC_PRECACHE_EXTENTS:
 		return f2fs_ioc_precache_extents(filp, arg);
 	case F2FS_IOC_RESIZE_FS:
@@ -3250,8 +3246,6 @@ long f2fs_compat_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 	case F2FS_IOC_GET_FEATURES:
 	case F2FS_IOC_FSGETXATTR:
 	case F2FS_IOC_FSSETXATTR:
-	case F2FS_IOC_GET_PIN_FILE:
-	case F2FS_IOC_SET_PIN_FILE:
 	case F2FS_IOC_PRECACHE_EXTENTS:
 	case F2FS_IOC_RESIZE_FS:
 #if F2FS_FS_SDP_ENCRYPTION
