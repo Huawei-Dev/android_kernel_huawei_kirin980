3b992d40cd6a230ffbd9ea3679da8604af8f35a2
Tetsuo Handa <penguin-kernel@i-love.sakura.ne.jp>
2021-12-18 18:41:56 +0900

    block: use "unsigned long" for blk_validate_block_size().
    commit 37ae5a0f5287a52cf51242e76ccf198d02ffe495 upstream.
    
    Since lo_simple_ioctl(LOOP_SET_BLOCK_SIZE) and ioctl(NBD_SET_BLKSIZE) pass
    user-controlled "unsigned long arg" to blk_validate_block_size(),
    "unsigned long" should be used for validation.
    
    Signed-off-by: Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Link: https://lore.kernel.org/r/9ecbf057-4375-c2db-ab53-e4cc0dff953d@i-love.sakura.ne.jp
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: David Hunter <david.hunter.linux@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit ee12aa483f6c8cecbd5a4c794867fee0e068b822)
    Signed-off-by: Vegard Nossum <vegard.nossum@oracle.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -72,7 +72,7 @@ struct keyslot_manager;
 #endif
 #endif
 
-static inline int blk_validate_block_size(unsigned int bsize)
+static inline int blk_validate_block_size(unsigned long bsize)
 {
 	if (bsize < 512 || bsize > PAGE_SIZE || !is_power_of_2(bsize))
 		return -EINVAL;
