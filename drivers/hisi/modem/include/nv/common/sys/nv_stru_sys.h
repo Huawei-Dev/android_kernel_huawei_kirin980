c541081382f7b5e3da516f7edbd7a4210049fc7e
Mashopy <eliasgheeraert@gmail.com>
2024-02-20 19:24:37 +0100

    hisi: modem: nv: Include int-ll64.h
    Change-Id: I97423b1e65bd99410e4dff4d19e7563e31b81346

--- drivers/hisi/modem/include/nv/common/sys/nv_stru_sys.h
+++ drivers/hisi/modem/include/nv/common/sys/nv_stru_sys.h
@@ -57,18 +57,7 @@ extern "C" {
 #endif
 
 #include "nv_id_sys.h"
-/*lint --e{761}*/
-#ifndef __u8_defined
-#define __u8_defined
-typedef signed char          s8;
-typedef unsigned char        u8;
-typedef signed short         s16;
-typedef unsigned short       u16;
-typedef signed int           s32;
-typedef unsigned int         u32;
-typedef signed long long     s64;
-typedef unsigned long long   u64;
-#endif
+#include <asm-generic/int-ll64.h>
 
 #define NV_ITEM_IMEI_SIZE 16
 
