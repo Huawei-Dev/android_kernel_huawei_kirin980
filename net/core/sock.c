fbcbc12e6ddd9bc1000e3a3c54c5ab962cb26f51
Peiyong Wang <wangpeiyong@xiaomi.corp-partner.google.com>
2024-03-05 01:46:22 +0000

    BACKPORT: net: core: enable SO_BINDTODEVICE for non-root users
    Currently, SO_BINDTODEVICE requires CAP_NET_RAW. This change allows a
    non-root user to bind a socket to an interface if it is not already
    bound. This is useful to allow an application to bind itself to a
    specific VRF for outgoing or incoming connections. Currently, an
    application wanting to manage connections through several VRF need to
    be privileged.
    
    Previously, IP_UNICAST_IF and IPV6_UNICAST_IF were added for
    Wine (76e21053b5bf3 and c4062dfc425e9) specifically for use by
    non-root processes. However, they are restricted to sendmsg() and not
    usable with TCP. Allowing SO_BINDTODEVICE would allow TCP clients to
    get the same privilege. As for TCP servers, outside the VRF use case,
    SO_BINDTODEVICE would only further restrict connections a server could
    accept.
    
    When an application is restricted to a VRF (with `ip vrf exec`), the
    socket is bound to an interface at creation and therefore, a
    non-privileged call to SO_BINDTODEVICE to escape the VRF fails.
    
    When an application bound a socket to SO_BINDTODEVICE and transmit it
    to a non-privileged process through a Unix socket, a tentative to
    change the bound device also fails.
    
    Before:
    
        >>> import socket
        >>> s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
        >>> s.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, b"dummy0")
        Traceback (most recent call last):
          File "<stdin>", line 1, in <module>
        PermissionError: [Errno 1] Operation not permitted
    
    After:
    
        >>> import socket
        >>> s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
        >>> s.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, b"dummy0")
        >>> s.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, b"dummy0")
        Traceback (most recent call last):
          File "<stdin>", line 1, in <module>
        PermissionError: [Errno 1] Operation not permitted
    
    Bug: 323792489
    Signed-off-by: Vincent Bernat <vincent@bernat.ch>
    Reviewed-by: David Ahern <dsahern@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit c427bfec18f2190b8f4718785ee8ed2db4f84ee6)
    Change-Id: Ie3f4c536b78da12dd961dc681c6dfb0cdf1a06b7
    Signed-off-by: Peiyong Wang <wangpeiyong@xiaomi.corp-partner.google.com>

--- net/core/sock.c
+++ net/core/sock.c
@@ -578,7 +578,7 @@ static int sock_setbindtodevice(struct sock *sk, char __user *optval,
 
 	/* Sorry... */
 	ret = -EPERM;
-	if (!ns_capable(net->user_ns, CAP_NET_RAW)
+	if (sk->sk_bound_dev_if && !ns_capable(net->user_ns, CAP_NET_RAW)
 #ifdef CONFIG_ANDROID_PARANOID_NETWORK
 		&& !in_egroup_p(AID_INET)
 #endif
