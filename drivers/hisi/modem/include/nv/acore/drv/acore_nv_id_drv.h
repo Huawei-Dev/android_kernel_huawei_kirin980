6d1f8695dc614f014a1c7b173607417f815feb32
Huawei_Dev <surdu_petru@yahoo.com>
2025-01-03 17:10:34 +0100

    hisi:modem: include linux/types.h

--- drivers/hisi/modem/include/nv/acore/drv/acore_nv_id_drv.h
+++ drivers/hisi/modem/include/nv/acore/drv/acore_nv_id_drv.h
@@ -56,39 +56,9 @@ extern "C" {
 #endif
 #endif
 
-//[NV elf????????]???????????? xuziheng/306651 20171207
 #ifndef NV_DEFINE
-
-#ifndef s8
-typedef signed char          s8;
+#include <linux/types.h>
 #endif
-#ifndef u8
-typedef unsigned char        u8;
-#endif
-#ifndef s16
-typedef signed short         s16;
-#endif
-#ifndef u16
-typedef unsigned short       u16;
-#endif
-#ifndef s32
-typedef signed int           s32;
-#endif
-#ifndef u32
-typedef unsigned int         u32;
-#endif
-#ifndef s64
-typedef signed long long     s64;
-#endif
-#ifndef u64
-typedef unsigned long long   u64;
-#endif
-
-#endif
-
-/*
- *  NV ID ????????????????????
- */
 
 typedef enum
 {
