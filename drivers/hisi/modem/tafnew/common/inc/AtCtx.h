2b854400b4bc6a391571571073aab57b95c4a999
Huawei_Dev <surdu_petru@yahoo.com>
2026-05-01 10:23:58 +0200

    modem: fixup invalide shift

--- drivers/hisi/modem/tafnew/common/inc/AtCtx.h
+++ drivers/hisi/modem/tafnew/common/inc/AtCtx.h
@@ -327,14 +327,18 @@ typedef VOS_UINT8 AT_CGREG_TYPE;
 #define AT_ARRAY_SIZE(a)                (sizeof((a)) / sizeof((a[0])))
 #endif
 
-/* ??port??????????????1 */
-#define AT_SET_BIT32(n)       ((VOS_UINT32)1 << (n))
+/* fixup invalide shift */
+#define AT_SET_BIT32(n) \
+	((VOS_UINT32)1U << ((n) & 31))
 
-#define AT_SET_BIT32_SELECT0(n)                (((n) < 32) ? AT_SET_BIT32(n):0)
+#define AT_SET_BIT32_SELECT0(n) \
+	(((n) < 32) ? ((VOS_UINT32)1U << ((n) & 31)) : 0)
 
-#define AT_SET_BIT32_SELECT1(n)                ((((n) >= 32)&&((n) < 64)) ? AT_SET_BIT32(n - 32):0)
+#define AT_SET_BIT32_SELECT1(n) \
+	((((n) >= 32) && ((n) < 64)) ? ((VOS_UINT32)1U << (((n) - 32) & 31)) : 0)
 
-#define AT_SET_BIT32_SELECT2(n)                ((((n) >= 64)&&((n) < 96)) ? AT_SET_BIT32(n - 64):0)
+#define AT_SET_BIT32_SELECT2(n) \
+	((((n) >= 64) && ((n) < 96)) ? ((VOS_UINT32)1U << (((n) - 64) & 31)) : 0)
 
 #define AT_CLIENT_CFG_ELEMENT(port) \
             { AT_CLIENT_ID_##port, TAF_NV_CLIENT_CFG_##port, #port, \
