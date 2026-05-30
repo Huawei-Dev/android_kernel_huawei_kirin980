a0462072f8d895048c6be3e8b008a6133ae2b253
Huawei_Dev <surdu_petru@yahoo.com>
2026-05-30 10:01:27 +0200

    tzdriver: fix TEE auth and enable hardware fingerprint

--- drivers/hisi/tzdriver/tc_client_driver.c
+++ drivers/hisi/tzdriver/tc_client_driver.c
@@ -191,6 +191,65 @@ static unsigned char teecd_hash[SHA256_DIGEST_LENTH] = {0};
 static unsigned char hidl_hash[SHA256_DIGEST_LENTH] = {0};
 static bool g_teecd_hash_enable = false;
 static bool g_hidl_hash_enable = false;
+
+static void dump_hash(const char *my_pkname, unsigned char *hash_buf)
+{
+	if (!hash_buf || !my_pkname) {
+		return;
+	}
+
+	TCDEBUG("SHA256 hash for %s:\n", my_pkname);
+	TCDEBUG("{0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, ",
+		*(hash_buf + 0), *(hash_buf + 1), *(hash_buf + 2), *(hash_buf + 3),
+		*(hash_buf + 4), *(hash_buf + 5), *(hash_buf + 6), *(hash_buf + 7));
+	TCDEBUG("0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, ",
+		*(hash_buf + 8), *(hash_buf + 9), *(hash_buf + 10), *(hash_buf + 11),
+		*(hash_buf + 12), *(hash_buf + 13), *(hash_buf + 14),
+		*(hash_buf + 15));
+	TCDEBUG("0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X,  ",
+		*(hash_buf + 16), *(hash_buf + 17), *(hash_buf + 18),
+		*(hash_buf + 19), *(hash_buf + 20), *(hash_buf + 21),
+		*(hash_buf + 22), *(hash_buf + 23));
+	TCDEBUG("0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X} ",
+		*(hash_buf + 24), *(hash_buf + 25), *(hash_buf + 26),
+		*(hash_buf + 27), *(hash_buf + 28), *(hash_buf + 29),
+		*(hash_buf + 30), *(hash_buf + 31));
+}
+
+static void apply_spoof_hash(const char *my_pkname,
+			     unsigned char *hash_buf,
+			     const unsigned char *src_hash)
+{
+	tlogd("Spoof now %s process\n", my_pkname);
+	memcpy(hash_buf, src_hash, MAX_SHA_256_SZ);
+}
+
+static void spoof_hash(const char *my_pkname, unsigned char *hash_buf)
+{
+	static const unsigned char keystore_hash[32] = {0xAA, 0x3B, 0x24, 0x94, 0xD7, 0xB8, 0x05, 0x42,
+					   0x34, 0x65, 0x7E, 0x10, 0x6A, 0xC8, 0x5B, 0x64,
+					   0xBD, 0xFE, 0x7F, 0x65, 0x77, 0xED, 0x26, 0x2F,
+					   0x15, 0x2A, 0x8A, 0x8C, 0x03, 0x1D, 0x81, 0x69};
+
+	static const unsigned char gatekeeper_hash[32] = {0xAF, 0x49, 0x6D, 0x17, 0x5E, 0x66, 0xC0, 0x45,
+					   0xEE, 0xFC, 0xC0, 0xA9, 0x0B, 0x04, 0x2E, 0xB2,
+					   0x32, 0x18, 0xA4, 0x9F, 0x73, 0xA3, 0x67, 0x29,
+					   0x16, 0xAD, 0x47, 0x90, 0x3F, 0x50, 0xA1, 0xA9};
+
+	static const unsigned char fingerprint_hash[32] = {0xAA, 0x27, 0xE1, 0xBF, 0x93, 0xB2, 0x5C, 0x3D,
+					    0x01, 0x69, 0xF2, 0x82, 0xA9, 0x4E, 0x3F, 0x4A,
+					    0x22, 0x55, 0x42, 0xD4, 0x64, 0x6B, 0x87, 0xBA,
+					    0x92, 0x91, 0xDA, 0x62, 0x1A, 0xCB, 0xDB, 0x59};
+
+	if (!strcmp(my_pkname, "/vendor/bin/hw/android.hardware.gatekeeper@1.0-service")) {
+		apply_spoof_hash(my_pkname, hash_buf, gatekeeper_hash);
+	} else if (!strcmp(my_pkname, "/vendor/bin/hw/android.hardware.keymaster@3.0-service")) {
+		apply_spoof_hash(my_pkname, hash_buf, keystore_hash);
+	} else if (!strcmp(my_pkname, "/vendor/bin/hw/vendor.huawei.hardware.biometrics.fingerprint@2.1-service")) {
+		apply_spoof_hash(my_pkname, hash_buf, fingerprint_hash);
+	}
+}
+
 /*
  * Calculate hash of task's text.
  * @cfc_rehash: if generate a random number and hash the resulting hash again.
@@ -1543,53 +1602,6 @@ static void remove_unused_session(TC_NS_Service *service,
 	put_session_struct(saved_session);
 }
 
-void dump_hash(char *my_pkname, unsigned char *hash_buf)
-{
-	TCDEBUG("SHA256 hash for %s:\n", my_pkname);
-	TCDEBUG("{0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, ",
-		*(hash_buf + 0), *(hash_buf + 1), *(hash_buf + 2), *(hash_buf + 3),
-		*(hash_buf + 4), *(hash_buf + 5), *(hash_buf + 6), *(hash_buf + 7));
-	TCDEBUG("0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, ",
-		*(hash_buf + 8), *(hash_buf + 9), *(hash_buf + 10), *(hash_buf + 11),
-		*(hash_buf + 12), *(hash_buf + 13), *(hash_buf + 14),
-		*(hash_buf + 15));
-	TCDEBUG("0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X,  ",
-		*(hash_buf + 16), *(hash_buf + 17), *(hash_buf + 18),
-		*(hash_buf + 19), *(hash_buf + 20), *(hash_buf + 21),
-		*(hash_buf + 22), *(hash_buf + 23));
-	TCDEBUG("0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X} ",
-		*(hash_buf + 24), *(hash_buf + 25), *(hash_buf + 26),
-		*(hash_buf + 27), *(hash_buf + 28), *(hash_buf + 29),
-		*(hash_buf + 30), *(hash_buf + 31));
-}
-
-void spoof_hash(char *my_pkname, unsigned char *hash_buf)
-{
-	unsigned char keystore_hash[32] = {0xAA, 0x3B, 0x24, 0x94, 0xD7, 0xB8, 0x05, 0x42,
-					   0x34, 0x65, 0x7E, 0x10, 0x6A, 0xC8, 0x5B, 0x64,
-					   0xBD, 0xFE, 0x7F, 0x65, 0x77, 0xED, 0x26, 0x2F,
-					   0x15, 0x2A, 0x8A, 0x8C, 0x03, 0x1D, 0x81, 0x69};
-
-	unsigned char gatekeeper_hash[32] = {0xAF, 0x49, 0x6D, 0x17, 0x5E, 0x66, 0xC0, 0x45,
-					   0xEE, 0xFC, 0xC0, 0xA9, 0x0B, 0x04, 0x2E, 0xB2,
-					   0x32, 0x18, 0xA4, 0x9F, 0x73, 0xA3, 0x67, 0x29,
-					   0x16, 0xAD, 0x47, 0x90, 0x3F, 0x50, 0xA1, 0xA9};
-
-	unsigned char fingerprint_hash[32] = {0x2F, 0x63, 0xF0, 0x29, 0x92, 0x51, 0x86, 0xB2,
-					    0xDF, 0xB3, 0xA3, 0x14, 0x15, 0xC3, 0xAD, 0x30,
-					    0x7E, 0x52, 0x75, 0x5A, 0xBC, 0x43, 0x7B, 0xAE,
-					    0x42, 0x3E, 0x9C, 0x38, 0xAB, 0x45, 0x52, 0xCB};
-
-	if (!strncmp(my_pkname, "/vendor/bin/hw/android.hardware.gatekeeper@1.0-service", 54))
-		memcpy(hash_buf, gatekeeper_hash, MAX_SHA_256_SZ);
-
-	if (!strncmp(my_pkname, "/vendor/bin/hw/android.hardware.keymaster@3.0-service", 53))
-		memcpy(hash_buf, keystore_hash, MAX_SHA_256_SZ);
-
-	if (!strncmp(my_pkname, "/vendor/bin/hw/vendor.huawei.hardware.biometrics.fingerprint@2.1-service", 72))
-		memcpy(hash_buf, fingerprint_hash, MAX_SHA_256_SZ);
-}
-
 int TC_NS_OpenSession(TC_NS_DEV_File *dev_file, TC_NS_ClientContext *context)
 {
 	int ret = -EINVAL;
@@ -1727,8 +1739,8 @@ find_service:
 	/* use the lock to make sure the TA sessions cannot be concurrency opened */
 	mutex_lock(&g_operate_session_lock);
 
-	dump_hash(dev_file->pkg_name, hash_buf);
-	spoof_hash(dev_file->pkg_name, hash_buf);
+	dump_hash((const char *)dev_file->pkg_name, hash_buf);
+	spoof_hash((const char *)dev_file->pkg_name, hash_buf);
 
 	/*cp hash_buf to global var, it is protected by lock */
 	ret = memcpy_s(g_ca_auth_hash_buf, (size_t)MAX_SHA_256_SZ,
