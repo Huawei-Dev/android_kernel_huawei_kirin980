0a776434e4128ff50cd1e1f87d314e1a26aa463c
Huawei_Dev <surdu_petru@yahoo.com>
2026-06-03 18:21:02 +0200

    huawei_platform: drop unused CONFIG_HUAWEI_OCP

--- drivers/video/fbdev/hisi/dss/hisi_fb.h
+++ drivers/video/fbdev/hisi/dss/hisi_fb.h
@@ -915,11 +915,6 @@ struct platform_device *hisi_fb_device_alloc(struct hisi_fb_panel_data *pdata,
 	uint32_t type, uint32_t id);
 struct platform_device *hisi_fb_add_device(struct platform_device *pdev);
 
-#ifdef CONFIG_HUAWEI_OCP
-int hisi_lcd_ocp_recover(struct notifier_block *nb,
-		unsigned long event, void *data);
-#endif
-
 #if CONFIG_SH_AOD_ENABLE
 /* sensorhub aod*/
 bool hisi_sensorhub_aod_hw_lock(struct hisi_fb_data_type *hisifd);
