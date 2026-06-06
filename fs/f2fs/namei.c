bd74a79aaac885a1eda702a4b80fd589caddd798
Huawei_Dev <surdu_petru@yahoo.com>
2026-06-06 14:18:39 +0200

    include: move acm_f2fs.h to hisi/

--- fs/f2fs/namei.c
+++ fs/f2fs/namei.c
@@ -22,7 +22,7 @@
 #include <trace/events/f2fs.h>
 
 #ifdef CONFIG_ACM
-#include <linux/acm_f2fs.h>
+#include <linux/hisi/acm_f2fs.h>
 #include <log/log_usertype.h>
 #define ACM_PHOTO	1
 #define ACM_VIDEO	2
